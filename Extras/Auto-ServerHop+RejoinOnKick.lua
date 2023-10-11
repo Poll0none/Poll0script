c8235a9601770423bd50aa00c38850f9ac09f229
Poll0none <126181078+Poll0none@users.noreply.github.com>
2023-10-11 16:32:09 +0200

    Update Auto-ServerHop+RejoinOnKick.lua

--- Extras/Auto-ServerHop+RejoinOnKick.lua
+++ Extras/Auto-ServerHop+RejoinOnKick.lua
@@ -1,19 +1,14 @@
 --Set getgenv().ServerHopperWait = math.random(10800, 14400) in your file before calling this script via loadstring
 
-
-    local Players = game:GetService("Players")
-    local Player = Players.LocalPlayer
-    local HttpService = game:GetService("HttpService")
-    local TeleportService = game:GetService("TeleportService")
-    local ServerList = HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. game.PlaceId .. '/servers/Public?sortOrder=Asc&limit=100'))
-
 local ServerHopper = function(time)
+    local ServerList = game:GetService("HttpService"):JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. game.PlaceId .. '/servers/Public?sortOrder=Asc&limit=100'))
+    
     if time then
         print("ServerHopper called and will jump to next server in: " .. time)
     wait(time)
         for i,v in next, ServerList.data do
             if v.playing >= 40 then
-                TeleportService:TeleportToPlaceInstance(game.PlaceId, v.id, Player)
+                game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, v.id, game:GetService("Players").LocalPlayer)
                 break
             end
         end
@@ -37,8 +32,7 @@ spawn(function()
         pcall(function()
 
                 --if child.Name == 'ErrorPrompt' and child:FindFirstChild('MessageArea') and child.MessageArea:FindFirstChild("ErrorFrame") then
-                    
-                
+
                 game:GetService("CoreGui").RobloxPromptGui.promptOverlay.ChildAdded:Connect(function(child)
                 if child.Name == 'ErrorPrompt' then
 
